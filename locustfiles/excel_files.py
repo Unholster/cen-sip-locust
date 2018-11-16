9394efce0af58e33bcc05e18cac37081781da140
Paula Rios <paula.rios.a@gmail.com>
2018-11-16 11:46:02 -0300

    API endpoints tasks for Excel

---
+++ locustfiles/excel_files.py
@@ -0,0 +1,31 @@
+import json
+from locust import HttpLocust, TaskSet, task
+
+
+def is_v1_endpoint(url):
+    return url.startswith('/api/v1/')
+
+
+class RequestExcelFiles(TaskSet):
+    endpoints = []
+    media_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
+    def on_start(self):
+        with open('endpoint_urls', 'r') as endpoints_file:
+            self.endpoints = json.load(endpoints_file)
+
+    @task
+    def api_v1(self):
+        for endpoint in self.endpoints:
+            if is_v1_endpoint(endpoint):
+                response = self.client.get(endpoint,
+                                           headers={'Origin': 'https://www.coordinador.cl',
+                                                    'Referer': 'https://www.coordinador.cl/',
+                                                    'Accept': self.media_type})
+                print(response.content)
+
+
+class WebsiteUser(HttpLocust):
+    task_set = RequestExcelFiles
+    min_wait = 0
+    max_wait = 3000
--- locustfiles/tsv_files.py
+++ locustfiles/tsv_files.py
@@ -6,35 +6,26 @@ def is_v1_endpoint(url):
     return url.startswith('/api/v1/')
 
 
-class UserBehavior(TaskSet):
+class RequestTSVFiles(TaskSet):
     endpoints = []
+    media_type = 'text/tab-separated-values-data'
 
     def on_start(self):
         with open('endpoint_urls', 'r') as endpoints_file:
             self.endpoints = json.load(endpoints_file)
 
-    @task(1)
-    def api_v1_json(self):
-        for endpoint in self.endpoints:
-            if is_v1_endpoint(endpoint):
-                response = self.client.get(endpoint,
-                                           headers={'Accept': 'application/json',
-                                                    'Origin': 'https://www.coordinador.cl',
-                                                    'Referer': 'https://www.coordinador.cl'})
-                print(response.content)
-
-    @task(1)
-    def api_v1_tsv(self):
+    @task
+    def api_v1(self):
         for endpoint in self.endpoints:
             if is_v1_endpoint(endpoint):
                 response = self.client.get(endpoint,
                                            headers={'Origin': 'https://www.coordinador.cl',
                                                     'Referer': 'https://www.coordinador.cl/',
-                                                    'Accept': 'text/tab-separated-values-data'})
+                                                    'Accept': self.media_type})
                 print(response.content)
 
 
 class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
+    task_set = RequestTSVFiles
     min_wait = 0
     max_wait = 3000
